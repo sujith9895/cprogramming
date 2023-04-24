194d4a0683f336ca9421fe3e49ffd9ae9fb50747
sujith9895 <sujithkrv9895@gmail.com>
2023-04-24 19:50:11 +0530

    pointers

---
+++ next.c
@@ -0,0 +1,21 @@
+//whether a number is positive or negative//
+
+#include <stdio.h>
+void main()
+{-
+    int num;
+   
+    printf("enter a number  \n");
+   
+    scanf("%d", &num);
+    
+    if (num >=0)
+    
+    
+    printf("%d is a positive number\n",num);
+    
+    else
+   
+    printf("%d is a negative number\n",num);
+
+}
---
+++ sujith.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("enter a number :");
+    scanf("%d", &n);
+
+    if(n % 2 == 0)
+    printf("%d is even number.", n);
+    else
+    printf("%d is odd number.", n);
+
+    return 0;
+}
